067e0feb0524aead6fba8e0af4187191e6a512ea
wangbin <wangbin_62@yahoo.com>
2016-05-28 01:23:27 +0800

    add simple authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :dbadmins
-  authenticate :dbadmin do
-    root to: "rails_db/dashboard#index"
-  end
+  root to: "rails_db/dashboard#index"
+  
 end
--- test/models/dbadmin_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class DbadminTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
